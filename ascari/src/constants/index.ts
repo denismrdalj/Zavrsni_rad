e356138796975417a1b6d33ae6a57ca282f68a9b
denismrdalj <124601001+denismrdalj@users.noreply.github.com>
2023-11-21 19:42:54 +0100

    Add files via upload

---
+++ ascari/src/constants/index.ts
@@ -0,0 +1,29 @@
+export const footerLinks = [
+    {
+      title: "About",
+      links: [
+        { title: "How it works", url: "/about" },
+        { title: "Featured", url: "/about" },
+        { title: "Partnership", url: "/about" },
+        { title: "Bussiness Relation", url: "/about" },
+      ],
+    },
+    {
+      title: "Company",
+      links: [
+        { title: "Events", url: "/about" },
+        { title: "Blog", url: "/about" },
+        { title: "Podcast", url: "/about" },
+        { title: "Invite a friend", url: "/about" },
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Discord", url: "/" },
+        { title: "Instagram", url: "/" },
+        { title: "Twitter", url: "/" },
+        { title: "Facebook", url: "/" },
+      ],
+    },
+  ];

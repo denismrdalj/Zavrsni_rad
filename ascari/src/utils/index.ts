58e686882aa2289b481b0755fc087959febdf5eb
denismrdalj <124601001+denismrdalj@users.noreply.github.com>
2023-11-21 19:45:10 +0100

    Add files via upload

---
+++ ascari/src/utils/index.ts
@@ -0,0 +1,16 @@
+import { CarType } from "../types/CarType"
+
+export const generateCarImageUrl = (car: CarType, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+} 
+
